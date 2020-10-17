d39a4e81c0788d9fbda00a0708c95bd2fbb7f112
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2020-10-17 19:45:45 +0200

    Game grid is now drawn in canvas
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- frontend/src/scripts/secondary.js
+++ frontend/src/scripts/secondary.js
@@ -1,7 +1,40 @@
 const Session = require("../../../libtowers/libtowers.js").Session;
 
+const GRID_SIZE = 100;
+const WIDTH = 800;
+const HEIGHT = 600;
+
+function draw_line(ctx, x1, y1, x2, y2) {
+    ctx.strokeStyle = 'rgba(0, 0, 0, 0.25)';
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+}
+
+function draw_background(ctx) {
+    ctx.fillStyle = 'rgb(244, 244, 244)';
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+}
+
+function draw_grid(ctx) {
+    for (let x = GRID_SIZE; x < WIDTH; x += GRID_SIZE) {
+        draw_line(ctx, x, 0, x, HEIGHT);
+    }
+    for (let y = GRID_SIZE; y < HEIGHT; y += GRID_SIZE) {
+        draw_line(ctx, 0, y, WIDTH, y);
+    }
+}
+
+function draw() {
+    const canvas = document.getElementById('towers_canvas');
+    const ctx = canvas.getContext('2d');
+
+    draw_grid(ctx);
+}
+
 function start() {
-    console.log("Towers started");
+    draw();
 }
 
 module.exports = {
