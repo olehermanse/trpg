f89e16892308ec2abf70e57180f2974f614383e4
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2020-10-17 18:32:55 +0200

    Removed text fields and buttons
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- frontend/src/scripts/main.js
+++ frontend/src/scripts/main.js
@@ -2,19 +2,5 @@
   let secondary = require("../../src/scripts/secondary.js");
   let Session = require("../../../libtowers/libtowers.js").Session;
 
-  const save_button = document.getElementById("save_button");
-  save_button.addEventListener("click", () => {
-    secondary.save_button_click();
-  });
-
-  const load_button = document.getElementById("load_button");
-  load_button.addEventListener("click", () => {
-    secondary.load_button_click();
-  });
-
-  window.addEventListener('popstate', (event) => {
-    secondary.history_back(event);
-  });
-
-  secondary.setup_id();
+  secondary.start();
 })();
