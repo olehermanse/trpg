f982a03093aec9ad6ac49c7f58b5087094c30b1e
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2020-10-18 17:22:13 +0200

    Made dev-server serve all the files
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- dev-server.js
+++ dev-server.js
@@ -1,12 +1,8 @@
-// This is identical to backend/server.js
-// except we use port 3000 and can add dev only things
-
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.sendFile("./index.html", { root: "frontend/dist" });
-});
+app.use(express.static("frontend/dist"));
+app.use(express.static("frontend/dist/scripts"));
 
 // Start server on port 3000:
 const port = 3000;
