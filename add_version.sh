23d661b3890403ad4a7721aed93e67a93ff63fbb
Ole Herman Schumacher Elgesem <ole@northern.tech>
2022-07-18 22:51:15 +0200

    Fixed git version commands in bash / sed script
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>

--- add_version.sh
+++ add_version.sh
@@ -1,6 +1,6 @@
 
-COMMIT_DATE=$(git log --pretty="%cd" --date=short "HEAD...HEAD~1")
-COMMIT_SHA=$(git log --pretty="%h" "HEAD...HEAD~1")
+COMMIT_DATE=$(git log --pretty="%cd" --date=short -n 1)
+COMMIT_SHA=$(git log --pretty="%h" -n 1)
 
 sed "s/COMMIT_DATE/$COMMIT_DATE/g" frontend/dist/index.html > tmp.html
 sed "s/COMMIT_SHA/$COMMIT_SHA/g" tmp.html > frontend/dist/index.html
