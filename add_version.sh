f3fb27a4cd9129519f43a2a1b7774be18866fa6e
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2023-03-08 00:11:58 +0100

    Fixed commit date and sha script after files were moved
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- add_version.sh
+++ add_version.sh
@@ -2,6 +2,6 @@
 COMMIT_DATE=$(git log --pretty="%cd" --date=short -n 1)
 COMMIT_SHA=$(git log --pretty="%h" -n 1)
 
-sed "s/COMMIT_DATE/$COMMIT_DATE/g" frontend/dist/index.html > tmp.html
-sed "s/COMMIT_SHA/$COMMIT_SHA/g" tmp.html > frontend/dist/index.html
+sed "s/COMMIT_DATE/$COMMIT_DATE/g" dist/index.html > tmp.html
+sed "s/COMMIT_SHA/$COMMIT_SHA/g" tmp.html > dist/index.html
 rm tmp.html
