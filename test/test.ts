8de935e17818fe4a48d9eb11db0b1d6c992640f8
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2023-05-20 04:03:57 +0200

    Switched to vitest instead of mocha
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- test/test.ts
+++ test/test.ts
@@ -1,163 +1,133 @@
 import { Game } from "../libtowers/libtowers.js";
-import { number_string } from "../libtowers/utils.js";
 
-import { strict as assert } from "assert";
+// example.test.js
+import { expect, test, describe } from "vitest";
 
-describe("Game", function () {
-  it("Can be created", function () {
+describe("Game", () => {
+  test("can be created with columns and rows", () => {
     let game = new Game(4, 3, null);
-    assert.strictEqual(game.rows, 3);
-    assert.strictEqual(game.columns, 4);
+    expect(game.rows).toBe(3);
+    expect(game.columns).toBe(4);
   });
-  describe("#is_empty()", function () {
-    it("Returns false for walls", function () {
+  describe("is_empty()", () => {
+    test("returns false for walls", () => {
       let game = new Game(5, 5, null);
-      assert.strictEqual(game.is_empty(0, 0), false);
+      expect(game.is_empty(0, 0)).toBe(false);
     });
-    it("Returns true for empty tiles", function () {
+    test("Returns true for empty tiles", () => {
       let game = new Game(5, 5, null);
-      assert.strictEqual(game.is_empty(1, 1), true);
+      expect(game.is_empty(1, 1)).toBe(true);
     });
-    it("Returns false for goal", function () {
+    test("Returns false for goal", () => {
       let game = new Game(5, 5, null);
       let c = game.goal.c;
       let r = game.goal.r;
-      assert.strictEqual(game.is_empty(c, r), false);
+      expect(game.is_empty(c, r)).toBe(false);
     });
-    it("Returns false for spawn", function () {
+    test("Returns false for spawn", () => {
       let game = new Game(5, 5, null);
       let c = game.spawn.c;
       let r = game.spawn.r;
-      assert.strictEqual(game.is_empty(c, r), false);
+      expect(game.is_empty(c, r)).toBe(false);
     });
   });
-  describe("#is_outside()", function () {
-    it("Returns false for positions inside", function () {
+  describe("is_outside()", () => {
+    test("returns false for positions inside", () => {
       let game = new Game(10, 20, null);
-      assert.strictEqual(game.is_outside(0, 0), false);
-      assert.strictEqual(game.is_outside(1, 1), false);
-      assert.strictEqual(game.is_outside(9, 19), false);
-      assert.strictEqual(game.is_outside(9, 1), false);
-      assert.strictEqual(game.is_outside(1, 19), false);
+      expect(game.is_outside(0, 0)).toBe(false);
+      expect(game.is_outside(1, 1)).toBe(false);
+      expect(game.is_outside(9, 19)).toBe(false);
+      expect(game.is_outside(9, 1)).toBe(false);
+      expect(game.is_outside(1, 19)).toBe(false);
     });
-    it("Returns true for positions outside", function () {
+    test("returns true for positions outside", () => {
       let game = new Game(10, 20, null);
-      assert.strictEqual(game.is_outside(-1, 0), true);
-      assert.strictEqual(game.is_outside(0, -1), true);
-      assert.strictEqual(game.is_outside(10, 20), true);
-      assert.strictEqual(game.is_outside(10, 0), true);
-      assert.strictEqual(game.is_outside(0, 20), true);
+      expect(game.is_outside(-1, 0)).toBe(true);
+      expect(game.is_outside(0, -1)).toBe(true);
+      expect(game.is_outside(10, 20)).toBe(true);
+      expect(game.is_outside(10, 0)).toBe(true);
+      expect(game.is_outside(0, 20)).toBe(true);
     });
   });
-  describe("#place_tower()", function () {
-    it("Places a tower", function () {
+  describe("place_tower()", () => {
+    test("places a tower", () => {
       let game = new Game(5, 5, null);
-      assert.strictEqual(game.towers.length, 0);
+      expect(game.towers.length).toBe(0);
       let p = game.find_empty_not_path();
       game.place_tower(p.c, p.r, "Gun tower");
-      assert.strictEqual(game.towers.length, 1);
+      expect(game.towers.length).toBe(1);
     });
   });
-  describe("#grid_click()", function () {
-    it("Places a tower", function () {
+  describe("grid_click()", () => {
+    test("places a tower", () => {
       let game = new Game(5, 5, null);
-      assert.strictEqual(game.towers.length, 0);
+      expect(game.towers.length).toBe(0);
       let p = game.find_empty_not_path();
-      assert.notStrictEqual(p, null);
+      expect(p).not.toBe(null);
       game.grid_click(p.c, p.r, "Gun tower");
-      assert.strictEqual(game.towers.length, 1);
+      expect(game.towers.length).toBe(1);
     });
-    it("Doesn't place a tower when dead", function () {
+    test("doesn't place a tower when dead", () => {
       let game = new Game(5, 5, null);
-      assert.strictEqual(game.towers.length, 0);
+      expect(game.towers.length).toBe(0);
       let p = game.find_empty_not_path();
       game.lives = 0;
       game.grid_click(p.c, p.r, "Gun tower");
-      assert.strictEqual(game.towers.length, 0);
+      expect(game.towers.length).toBe(0);
     });
-    it("Doesn't block the path", function () {
+    test("doesn't block the path", () => {
       let game = new Game(3, 3, null);
-      assert.strictEqual(game.towers.length, 0);
+      expect(game.towers.length).toBe(0);
       let row = game.spawn.r;
       game.grid_click(1, row, "Gun tower");
-      assert.strictEqual(game.towers.length, 0);
-      assert.strictEqual(game.is_empty(1, row), true);
+      expect(game.towers.length).toBe(0);
+      expect(game.is_empty(1, row)).toBe(true);
     });
-    it("Doesn't place a tower on a wall", function () {
+    test("doesn't place a tower on a wall", () => {
       let game = new Game(5, 5, null);
-      assert.strictEqual(game.towers.length, 0);
+      expect(game.towers.length).toBe(0);
       game.grid_click(0, 0, "Gun tower");
-      assert.strictEqual(game.towers.length, 0);
+      expect(game.towers.length).toBe(0);
     });
-    it("Doesn't place a tower on a tower", function () {
+    test("doesn't place a tower on a tower", () => {
       let game = new Game(5, 5, null);
-      assert.strictEqual(game.towers.length, 0);
+      expect(game.towers.length).toBe(0);
       let p = game.find_empty_not_path();
       game.grid_click(p.c, p.r, "Gun tower");
-      assert.strictEqual(game.towers.length, 1);
+      expect(game.towers.length).toBe(1);
       game.grid_click(p.c, p.r, "Gun tower");
-      assert.strictEqual(game.towers.length, 1);
+      expect(game.towers.length).toBe(1);
     });
-    it("Doesn't place a tower when you have no money", function () {
+    test("doesn't place a tower when you have no money", () => {
       let game = new Game(5, 5, null);
       game.money = 0;
-      assert.strictEqual(game.towers.length, 0);
+      expect(game.towers.length).toBe(0);
       let row = game.is_path(1, 1) ? 2 : 1;
       game.grid_click(1, row, "Gun tower");
-      assert.strictEqual(game.towers.length, 0);
+      expect(game.towers.length).toBe(0);
     });
   });
-  describe("#is_empty()", function () {
-    it("Returns true by default", function () {
+  describe("is_empty()", () => {
+    test("Game.is_empty() returns true by default", () => {
       let game = new Game(4, 3, null);
-      assert.strictEqual(game.is_empty(1, 1), true);
+      expect(game.is_empty(1, 1)).toBe(true);
     });
-    it("Returns false for wall", function () {
+
+    test("Game.is_empty() returns false for wall", () => {
       let game = new Game(10, 10, null);
-      assert.strictEqual(game.is_empty(0, 0), false);
-      assert.strictEqual(game.tiles[0][0], "wall");
+      expect(game.is_empty(0, 0)).toBe(false);
+      expect(game.tiles[0][0], "wall");
     });
   });
-  describe("#can_afford()", function () {
-    it("Returns true at beginning of game", function () {
+  describe("can_afford()", () => {
+    test("returns true at beginning of game", () => {
       let game = new Game(4, 3, null);
-      assert.strictEqual(game.can_afford("Gun tower"), true);
+      expect(game.can_afford("Gun tower")).toBe(true);
     });
-    it("Returns false when you have no money", function () {
+    test("returns false when you have no money", () => {
       let game = new Game(4, 3, null);
       game.money = 0;
-      assert.strictEqual(game.can_afford("Gun tower"), false);
+      expect(game.can_afford("Gun tower")).toBe(false);
     });
-  });
-});
-
-describe("number_string", function () {
-  it("inserts spaces", function () {
-    assert.strictEqual(number_string(1000), "1 000");
-    assert.strictEqual(number_string(123456), "123 456");
-    assert.strictEqual(number_string(999000), "999 000");
-    assert.strictEqual(number_string(1000000), "1 000 000");
-    assert.strictEqual(number_string(9999999), "9 999 999");
-  });
-  it("works for zero", function () {
-    assert.strictEqual(number_string(0), "0");
-  });
-  it("works for small numbers", function () {
-    assert.strictEqual(number_string(1), "1");
-    assert.strictEqual(number_string(2), "2");
-    assert.strictEqual(number_string(3), "3");
-    assert.strictEqual(number_string(99), "99");
-    assert.strictEqual(number_string(100), "100");
-    assert.strictEqual(number_string(999), "999");
-  });
-  it("works for negative numbers", function () {
-    assert.strictEqual(number_string(-1), "-1");
-    assert.strictEqual(number_string(-2), "-2");
-    assert.strictEqual(number_string(-3), "-3");
-    assert.strictEqual(number_string(-99), "-99");
-    assert.strictEqual(number_string(-100), "-100");
-    assert.strictEqual(number_string(-999), "-999");
-    assert.strictEqual(number_string(-1000), "-1 000");
-    assert.strictEqual(number_string(-9999), "-9 999");
   });
 });
