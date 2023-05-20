9c1ee893dc3277834f2891e7a394af37012b393d
Ole Herman Schumacher Elgesem <ole@northern.tech>
2023-05-20 19:39:53 +0200

    Kind of working with node and express
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>

--- src/backend/backend.ts
+++ src/backend/backend.ts
@@ -1,10 +1,16 @@
-import express from "express";
+const express = require("express");
 const app = express();
+
+process.on('SIGINT', function() {
+  console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
+  // some other closing procedures go here
+  process.exit(0);
+});
 
 app.use(express.static("dist"));
 
-// Start server on port 80:
-const port = 80;
+// Start server on port 3000:
+const port = 3000;
 app.listen(port, function () {
   console.log("tpg dev-server listening on port " + port);
 });
