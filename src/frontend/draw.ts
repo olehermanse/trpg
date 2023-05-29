fb46c70b182a692ac7b7024b10f697770dcf8e9b
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2023-05-29 22:48:56 +0200

    draw.ts: Added typescript types
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- src/frontend/draw.ts
+++ src/frontend/draw.ts
@@ -2,31 +2,52 @@ const PI = 3.14159;
 const LINE_RATIO = 0.1;
 const SCALE = window.devicePixelRatio;
 
-function sxy(x, y) {
-  const obj : any = {};
-  obj.x = x * SCALE;
-  obj.y = y * SCALE;
+interface XY {
+  x: number;
+  y: number;
+}
+
+interface XYR extends XY {
+  r: number;
+}
+
+interface XYWH extends XY {
+  w: number;
+  h: number;
+}
+
+function sxy(x: number, y: number):XY {
+  const obj: XY = {
+    x : x * SCALE,
+    y : y * SCALE,
+  };
   return obj;
 }
 
-function sxyr(x, y, r) {
-  const obj : any = {};
-  obj.x = x * SCALE;
-  obj.y = y * SCALE;
-  obj.r = r * SCALE;
+function sxyr(x: number, y: number, r: number):XYR {
+  const obj: XYR = {
+    x: x * SCALE,
+    y: y * SCALE,
+    r: r * SCALE,
+  };
   return obj;
 }
 
-function sxywh(x, y, w, h) {
-  const obj : any = {};
-  obj.x = x * SCALE;
-  obj.y = y * SCALE;
-  obj.w = w * SCALE;
-  obj.h = h * SCALE;
+function sxywh(x: number, y: number, w: number, h: number):XYWH {
+  const obj: XYWH = {
+    x: x * SCALE,
+    y: y * SCALE,
+    w: w * SCALE,
+    h: h * SCALE,
+  };
   return obj;
 }
 
-function _setLineWidth(ctx, r, lineWidth) {
+function _setLineWidth(
+  ctx: CanvasRenderingContext2D,
+  r: number,
+  lineWidth: number
+) {
   if (lineWidth === null) {
     ctx.lineWidth = Math.round(r * LINE_RATIO);
   } else {
@@ -34,7 +55,15 @@ function _setLineWidth(ctx, r, lineWidth) {
   }
 }
 
-function _line(ctx, x1, y1, x2, y2, strokeStyle, lineWidth) {
+function _line(
+  ctx: CanvasRenderingContext2D,
+  x1: number,
+  y1: number,
+  x2: number,
+  y2: number,
+  strokeStyle: string,
+  lineWidth: number
+) {
   ctx.strokeStyle = strokeStyle;
   ctx.lineWidth = lineWidth;
   ctx.beginPath();
@@ -43,13 +72,29 @@ function _line(ctx, x1, y1, x2, y2, strokeStyle, lineWidth) {
   ctx.stroke();
 }
 
-function line(ctx, x1, y1, x2, y2, strokeStyle, lineWidth) {
+function line(
+  ctx: CanvasRenderingContext2D,
+  x1: number,
+  y1: number,
+  x2: number,
+  y2: number,
+  strokeStyle: string,
+  lineWidth: number
+) {
   const a = sxy(x1, y1);
   const b = sxy(x2, y2);
   _line(ctx, a.x, a.y, b.x, b.y, strokeStyle, lineWidth);
 }
 
-function _circle(ctx, x, y, r, fill = null, stroke = null, lineWidth = null) {
+function _circle(
+  ctx: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  r: number,
+  fill: string | null = null,
+  stroke: string | null = null,
+  lineWidth: number | null = null
+) {
   _setLineWidth(ctx, r, lineWidth);
   ctx.beginPath();
   ctx.arc(x, y, r, 0, 2 * PI);
@@ -63,12 +108,29 @@ function _circle(ctx, x, y, r, fill = null, stroke = null, lineWidth = null) {
   }
 }
 
-function circle(ctx, x, y, r, fill = null, stroke = null, lineWidth = null) {
+function circle(
+  ctx: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  r: number,
+  fill: string | null = null,
+  stroke: string | null = null,
+  lineWidth: number | null = null
+) {
   const c = sxyr(x, y, r);
   _circle(ctx, c.x, c.y, c.r, fill, stroke, lineWidth);
 }
 
-function _triangle(ctx, x, y, r, angle, fill, stroke = null, lineWidth = null) {
+function _triangle(
+  ctx: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  r: number,
+  angle: number,
+  fill: string,
+  stroke: string | null = null,
+  lineWidth: number | null = null
+) {
   _setLineWidth(ctx, r, lineWidth);
 
   // Matrix transformation
@@ -94,20 +156,29 @@ function _triangle(ctx, x, y, r, angle, fill, stroke = null, lineWidth = null) {
   ctx.setTransform(1, 0, 0, 1, 0, 0);
 }
 
-function triangle(ctx, x, y, r, angle, fill, stroke = null, lineWidth = null) {
+function triangle(
+  ctx: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  r: number,
+  angle: number,
+  fill: string,
+  stroke: string | null = null,
+  lineWidth: number | null = null
+) {
   const t = sxyr(x, y, r);
   _triangle(ctx, t.x, t.y, t.r, angle, fill, stroke, lineWidth);
 }
 
 function _rectangle(
-  ctx,
-  x,
-  y,
-  w,
-  h,
-  fill = null,
-  stroke = null,
-  lineWidth = null
+  ctx: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  w: number,
+  h: number,
+  fill: string | null = null,
+  stroke: string | null = null,
+  lineWidth: number | null = null
 ) {
   _setLineWidth(ctx, w / 2, lineWidth);
 
@@ -122,29 +193,50 @@ function _rectangle(
 }
 
 function rectangle(
-  ctx,
-  x,
-  y,
-  w,
-  h,
-  fill = null,
-  stroke = null,
-  lineWidth = null
+  ctx: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  w: number,
+  h: number,
+  fill: string | null = null,
+  stroke: string | null = null,
+  lineWidth: number | null = null
 ) {
   const r = sxywh(x, y, w, h);
   _rectangle(ctx, r.x, r.y, r.w, r.h, fill, stroke, lineWidth);
 }
 
-function _image(ctx, img, x, y, w, h) {
+function _image(
+  ctx: CanvasRenderingContext2D,
+  img: HTMLImageElement,
+  x: number,
+  y: number,
+  w: number,
+  h: number
+) {
   ctx.drawImage(img, x, y, w, h);
 }
 
-function image(ctx, img, x, y, w, h) {
+function image(
+  ctx: CanvasRenderingContext2D,
+  img: HTMLImageElement,
+  x: number,
+  y: number,
+  w: number,
+  h: number
+) {
   const i = sxywh(x, y, w, h);
   _image(ctx, img, i.x, i.y, i.w, i.h);
 }
 
-function _text(ctx, x, y, string, c, size) {
+function _text(
+  ctx: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  string: string,
+  c: string,
+  size: number
+) {
   if (string.includes("\n")) {
     const strings = string.split("\n");
     for (let s of strings) {
@@ -161,40 +253,77 @@ function _text(ctx, x, y, string, c, size) {
   ctx.fillText(string, x, y);
 }
 
-function text(ctx, x, y, string, c, size) {
+function text(
+  ctx: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  string: string,
+  c: string,
+  size: number
+) {
   ctx.textAlign = "center";
   ctx.textBaseline = "middle";
   const p = sxy(x, y);
   _text(ctx, p.x, p.y, string, c, SCALE * size);
 }
 
-function text_bottom_right(ctx, x, y, string, c, size) {
+function text_bottom_right(
+  ctx: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  string: string,
+  c: string,
+  size: number
+) {
   ctx.textAlign = "right";
   ctx.textBaseline = "bottom";
   const p = sxy(x, y);
   _text(ctx, p.x, p.y, string, c, SCALE * size);
 }
 
-function text_top_left(ctx, x, y, string, c, size) {
+function text_top_left(
+  ctx: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  string: string,
+  c: string,
+  size: number
+) {
   ctx.textAlign = "left";
   ctx.textBaseline = "top";
   const p = sxy(x, y);
   _text(ctx, p.x, p.y, string, c, SCALE * size);
 }
 
-function fill_text(ctx, string, x, y, c, font, textAlign, textBaseline) {
+function fill_text(
+  ctx: CanvasRenderingContext2D,
+  string: string,
+  x: number,
+  y: number,
+  c: string,
+  font: number,
+  textAlign: string,
+  textBaseline: string
+) {
   x = x * SCALE;
   y = y * SCALE;
   ctx.font = "" + Math.floor(SCALE * font) + "px monospace";
-  ctx.textAlign = textAlign;
-  ctx.textBaseline = textBaseline;
+  ctx.textAlign = textAlign as CanvasTextAlign;
+  ctx.textBaseline = textBaseline as CanvasTextBaseline;
   ctx.fillStyle = c;
   ctx.fillText(string, x, y);
 }
 
 const GRID_COLOR = "rgba(200,200,200,0.5)";
 
-function grid(ctx, size, x0, y0, width, height) {
+function grid(
+  ctx: CanvasRenderingContext2D,
+  size: number,
+  x0: number,
+  y0: number,
+  width: number,
+  height: number
+) {
   for (let x = size; x < width; x += size) {
     line(ctx, x, y0, x, y0 + height, GRID_COLOR, size / 25);
   }
@@ -210,7 +339,15 @@ const red = "#ff0000";
 const white = "#ffffff";
 const grey = "#666666";
 
-function healthbar(ctx, x, y, w, h, current, max) {
+function healthbar(
+  ctx: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  w: number,
+  h: number,
+  current: number,
+  max: number
+) {
   const ratio = current / max;
   x -= w / 2;
   y -= h / 2;
