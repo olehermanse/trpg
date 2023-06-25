cefbf62fdbef7fcfcb872ddefd25ec9b6b27fe55
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2023-06-25 02:27:58 +0200

    Switched backend to deno instead of node

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,27 @@ FROM node:18 AS build
 WORKDIR /towers
 COPY package-lock.json package.json ./
 RUN npm install --only=prod
-COPY . ./
+COPY .git .git
+COPY src src
+COPY public public
+COPY add_version.sh add_version.sh
+COPY tsconfig.json tsconfig.json
+COPY vite.config.js vite.config.js
+COPY index.html index.html
 RUN rm -rf dist
 RUN npm run build
+RUN bash add_version.sh
+
+FROM node:18 AS test
+WORKDIR /towers
+COPY --from=build /towers /towers
+COPY test test
 RUN npm install
 RUN npm run test
-RUN bash add_version.sh
-CMD [ "node" , "dist/backend/backend.js"]
+
+FROM denoland/deno:1.34.3 AS run
+WORKDIR /towers
+COPY --from=build /towers/dist/ dist/
+COPY src/ src/
+COPY --from=test /towers/package.json /towers/package.json
+CMD [ "deno" , "run", "--allow-net", "--allow-read", "--allow-env", "src/backend/backend.ts"]
--- src/backend/backend.ts
+++ src/backend/backend.ts
@@ -1,16 +1,66 @@
-const express = require("express");
-const app = express();
+// Start listening on port 3000 of localhost.
+const server = Deno.listen({ port: 3000 });
+console.log("Backend running on http://localhost:3000/");
 
-process.on('SIGINT', function() {
-  console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
-  // some other closing procedures go here
-  process.exit(0);
-});
+for await (const conn of server) {
+  handleHttp(conn).catch(console.error);
+}
 
-app.use(express.static("dist"));
+async function handleHttp(conn: Deno.Conn) {
+  const httpConn = Deno.serveHttp(conn);
+  for await (const requestEvent of httpConn) {
+    // Use the request pathname as filepath
+    const url = new URL(requestEvent.request.url);
+    let filepath = decodeURIComponent(url.pathname);
 
-// Start server on port 3000:
-const port = 3000;
-app.listen(port, function () {
-  console.log("towers dev-server listening on port " + port);
-});
+    if (
+      !filepath.startsWith("/") ||
+      filepath.includes("..") ||
+      filepath.includes("//") ||
+      filepath.includes(" ") ||
+      filepath.includes(";") ||
+      filepath.includes(",") ||
+      filepath.includes("'") ||
+      filepath.includes('"') ||
+      filepath.includes("*")
+    ) {
+      const notFoundResponse = new Response("404 Not Found", { status: 404 });
+      await requestEvent.respondWith(notFoundResponse);
+    }
+
+    if (filepath === "/") {
+      filepath = "/index.html";
+    }
+
+    let headers = {};
+    if (filepath === "/index.html") {
+      headers["content-type"] = "text/html";
+    } else if (filepath === "/favicon.ico") {
+      headers["content-type"] = "image/x-icon";
+    } else if (filepath.endsWith(".js")) {
+      headers["content-type"] = "application/javascript";
+    } else if (filepath.endsWith(".css")) {
+      headers["content-type"] = "text/css";
+    } else {
+      const notFoundResponse = new Response("404 Not Found", { status: 404 });
+      await requestEvent.respondWith(notFoundResponse);
+    }
+
+    // Try opening the file
+    let file;
+    try {
+      file = await Deno.open(`./dist${filepath}`, { read: true });
+    } catch {
+      // If the file cannot be opened, return a "404 Not Found" response
+      const notFoundResponse = new Response("404 Not Found", { status: 404 });
+      await requestEvent.respondWith(notFoundResponse);
+      continue;
+    }
+
+    // Build a readable stream so the file doesn't have to be fully loaded into
+    // memory while we send it
+    const readableStream = file.readable;
+    const response = new Response(readableStream, { headers: headers });
+    await requestEvent.respondWith(response);
+  }
+}
