28f23e1cf267c8e25de9167dffe0046ac033d6f4
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2024-06-23 00:34:12 +0200

    Added rectangle drawing
    Signed-off-by: Ole Herman Schumacher Elgesem <oleherman93@gmail.com>

--- src/todo_utils.ts
+++ src/todo_utils.ts
@@ -1,8 +1,6 @@
 import { WH } from "@olehermanse/utils/funcs.js";
 import { XY } from "@olehermanse/utils";
 
-const LINE_RATIO = 0.1;
-
 function SCALE() {
   // This should be a constant, but I want this file to be importable by deno,
   // so not using a global variable for this.
@@ -12,40 +10,15 @@ function SCALE() {
 type Canvas = HTMLCanvasElement | OffscreenCanvas;
 type Context = CanvasRenderingContext2D | OffscreenCanvasRenderingContext2D;
 
-function _set_line_width(
-  ctx: Context,
-  r: number,
-  lineWidth?: number | null,
-) {
-  if (lineWidth === null || lineWidth === undefined) {
-    ctx.lineWidth = Math.round(r * LINE_RATIO);
-  } else {
-    ctx.lineWidth = lineWidth;
-  }
+/*
+function _set_pixel(img: ImageData, x: number, y: number, r: number, g: number, b: number, a: number){
+  const i = (x + y*img.width) * 4;
+  img.data[i] = r;
+  img.data[i+1] = g;
+  img.data[i+2] = b;
+  img.data[i+3] = a;
 }
-
-function _rectangle(
-  ctx: Context,
-  x: number,
-  y: number,
-  w: number,
-  h: number,
-  fill: string | null,
-  stroke?: string | null,
-  lineWidth?: number,
-) {
-  _set_line_width(ctx, w / 2, lineWidth);
-
-  if (fill) {
-    ctx.fillStyle = fill;
-    ctx.fillRect(x, y, w, h);
-  }
-  if (stroke) {
-    ctx.strokeStyle = stroke;
-    ctx.strokeRect(x, y, w, h);
-  }
-}
-
+*/
 export class Drawer<T extends Canvas> {
   ctx: Context;
   canvas: T;
@@ -69,34 +42,60 @@ export class Drawer<T extends Canvas> {
     ctx.drawImage(sprite, 0, 0);
     ctx.restore();
   }
+  clear() {
+    const width = this.canvas.width;
+    const height = this.canvas.height;
+    const rect = this.ctx.createImageData(width, height);
+    // Each pixel is 4 8 bit int (RGBA)
+    // Defaults to all zeroes (all black and transparent)
+    // Loop through all pixels, removing the transparency:
+    for (let i = 0; i < width * height; i++) {
+      rect.data[3 + i * 4] = 255; // Set alpha to 100%, not transparent
+      // Leave other pixels alone, they are black.
+    }
+    this.ctx.putImageData(rect, 0, 0);
+  }
   rectangle(
     pos: XY,
     size: WH,
-    fill: string | null,
-    stroke?: string,
-    lineWidth?: number,
   ) {
+    let x = pos.x;
+    let y = pos.y;
+    let width = size.width;
+    let height = size.height;
     if (this.autoscale) {
-      return _rectangle(
-        this.ctx,
-        SCALE() * pos.x,
-        SCALE() * pos.y,
-        SCALE() * size.width,
-        SCALE() * size.height,
-        fill,
-        stroke,
-        lineWidth ? SCALE() * lineWidth : undefined,
-      );
+      x = x * SCALE();
+      y = y * SCALE();
+      width = width * SCALE();
+      height = height * SCALE();
     }
-    return _rectangle(
-      this.ctx,
-      pos.x,
-      pos.y,
-      size.width,
-      size.height,
-      fill,
-      stroke,
-      lineWidth,
-    );
+    const left = this.ctx.createImageData(1, height);
+    const right = this.ctx.createImageData(1, height);
+    const top = this.ctx.createImageData(width, 1);
+    const bottom = this.ctx.createImageData(width, 1);
+    for (let i = 0; i < width; i++) {
+      top.data[i * 4 + 0] = 255;
+      top.data[i * 4 + 1] = 255;
+      top.data[i * 4 + 2] = 255;
+      top.data[i * 4 + 3] = 255;
+      bottom.data[i * 4 + 0] = 255;
+      bottom.data[i * 4 + 1] = 255;
+      bottom.data[i * 4 + 2] = 255;
+      bottom.data[i * 4 + 3] = 255;
+    }
+    for (let i = 0; i < height; i++) {
+      left.data[i * 4 + 0] = 255;
+      left.data[i * 4 + 1] = 255;
+      left.data[i * 4 + 2] = 255;
+      left.data[i * 4 + 3] = 255;
+      right.data[i * 4 + 0] = 255;
+      right.data[i * 4 + 1] = 255;
+      right.data[i * 4 + 2] = 255;
+      right.data[i * 4 + 3] = 255;
+    }
+    this.ctx.putImageData(top, x, y);
+    this.ctx.putImageData(left, x, y);
+    this.ctx.putImageData(right, x + width - 1, y);
+    this.ctx.putImageData(bottom, x, y + height - 1);
   }
 }
