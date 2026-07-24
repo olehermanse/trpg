73e9d74d531015d3296c5e893207828937e531e5
Ole Herman Schumacher Elgesem <oleherman93@gmail.com>
2026-07-24 22:09:55 +0200

    Tweaks to sizes, radius, speed

--- src/frontend/entrypoint.ts
+++ src/frontend/entrypoint.ts
@@ -2,22 +2,22 @@ import { Application } from "./application.ts";
 
 function start(canvas: HTMLCanvasElement) {
   let scale = window.devicePixelRatio;
-  let columns = 12;
-  let rows = 9;
+  let columns = 16;
+  let rows = 12;
   const ctx = canvas.getContext("2d");
   const application: Application = new Application(
     canvas,
     ctx,
     columns,
     rows,
-    12 * 16,
-    9 * 16,
+    columns * 16,
+    rows * 16,
     scale,
   );
-  canvas.style.width = `${application.width * 4}px`;
+  // canvas.style.width = `${application.width * 4}px`;
   canvas.style.height = `${application.height * 4}px`;
-  canvas.width = 12 * 16;
-  canvas.height = 9 * 16;
+  canvas.width = columns * 16;
+  canvas.height = rows * 16;
   // application.setup_events(canvas, on_start_click, on_victory);
   const ms = 10;
   window.setInterval(() => {
